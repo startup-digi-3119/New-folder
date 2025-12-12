96f9b09e23cf9af2ae54024fa0d82c3dd1745170
Startup.Updated <startupmenswear.digi@gmail.com>
2025-12-12 21:17:33 +0530

    Fix Map Embed to point to Coimbatore Address

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ export default function HomePage() {
                         <div className="relative h-[400px] bg-gray-200 rounded-2xl overflow-hidden shadow-xl border-4 border-white">
                             {/* Google Maps Embed */}
                             <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.973715893693!2d80.2376!3d13.037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTPCsDAyJzEzLjIiTiA4MMKwMTQnMTUuNCJF!5e0!3m2!1sen!2sin!4v1635760000000!5m2!1sen!2sin"
+                                src="https://maps.google.com/maps?q=Startup%20Menswear%2C%20140%2C%20141%2F1%2C%20Uppilipalayam%20Main%20Road%2C%20Coimbatore&t=&z=15&ie=UTF8&iwloc=&output=embed"
                                 width="100%"
                                 height="100%"
                                 style={{ border: 0 }}
